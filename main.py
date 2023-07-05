98ff68dfb44c3d2b415da40189ff280311652424
Jaxon <jaxondlee@gmail.com>
2023-07-05 11:05:39 -0700

    Make small improvements

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 Guide to interfacing with robot-
     car = NvidiaRacecar()  : initializes car, you can control it w/ "car" object 
     car.throttle = [-1.0, -0.15] U [0.15, 1.0]      : sets motor speed
-    car.steering = [-1.0, 1.0]                      : sets motor steering    
+    car.steering = [-1.0, 0.0) U (0.0, 1.0]         : sets motor steering    
      
     actual_throttle = car.throttle_gain * car.throttle
     actual steering = car.steering_gain * car.steering + car.steering_offset
@@ -22,36 +22,40 @@ __email__ = "jaxondlee@gmail.com"
 __status__ = "Development"
 
 
-def setup():
+def setup(car : NvidiaRacecar):
     """Force motor encoders to turn on and set steering to initial position.
     """
-    temp_car = NvidiaRacecar()
-    # Takes a few seconds
-    #temp_car.steering = 1.0
-    temp_car.throttle = 0.2
+    car.steering_offset = -0.2
+    #car.throttle = 0.2
+    #time.sleep(0.5)
+    car.throttle = 0.0
     time.sleep(0.5)
-    temp_car.throttle = 0.0
-    time.sleep(0.5)
+    
     # This should turn the motor encoder on
+    return car
     
     
-def do_spiral_method(car):    
+def do_spiral_method(car : NvidiaRacecar):    
     """Apply the spiral method for traversing ARL location.
 
     Args:
         car (NvidiaRacecar): robot object
     """
-    car.steering = 1.0
+    car.steering = -0.15
     time.sleep(1)
     print(car.throttle_gain)
-    car.throttle = 0.15
-    
+    car.throttle = 0.175
+    while (True):
+        pass
     
     start_time = time.time()
     
-    while (car.steering > 0.0 and time.time() - start_time < 20):
-        #car.steering -= 0.005
+    while (car.steering < 0.0 and time.time() - start_time < 30):
+        car.steering += 0.005
         time.sleep(0.5)
+    
+    if (time.time() - start_time >= 30):
+        print("Timeout reached.")
     
 
 def main():
@@ -60,12 +64,13 @@ def main():
     """
     # Initial setup
     print("Hello World!")
-    setup()
+    car = NvidiaRacecar()  # Takes a few seconds
+    setup(car)
+    print("Set up!")
+    
     
     # Main code
-    car = NvidiaRacecar()
-    print("Set up!")
-    # Do this so that car throttle is set to 0 at the end.
+    ## Do this so that car throttle is set to 0 at the end.
     try:
         do_spiral_method(car)
     except KeyboardInterrupt:
@@ -75,6 +80,7 @@ def main():
     
     # Closing things
     print("Done")
+    car.steering = 0.0001
     car.throttle = 0.0
     print("Goodbye World... Until next time!")
     
